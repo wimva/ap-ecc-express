da1ca5ad8014b7a653687019134e1e8f55c215c8
Wim Van Aerschot <wim.van.aerschot@gmail.com>
2024-10-15 20:29:48 +0200

    fix https

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://ap-ecc-express.onrender.com/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
   console.log(profile);
