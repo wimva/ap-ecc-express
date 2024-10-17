8535625bd408aa6de60b220317268ba2654e9170
Wim Van Aerschot <wim.van.aerschot@gmail.com>
2024-10-17 15:32:11 +0200

    pls last whoopsie

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://ap-ecc-express.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   console.log(profile);
   try {
