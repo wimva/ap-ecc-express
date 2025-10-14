3d39e6900b7ad875c3e553bb45a35e86d7ea44bb
Van Aerschot Wim <vanaerschotwim@CLP-20-395.alpaca.int>
2025-10-14 09:32:20 +0200

    remove google auth

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import session from 'express-session';
 import User from './models/User.js';
 import testRoute from './routes/test.js';
 import indexRoute from './routes/index.js';
@@ -17,57 +14,6 @@ const app = express();
 
 app.use(express.json());
 
-// Configure session middleware
-app.use(
-  session({
-    store: mongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-    },
-  }),
-);
-
-// Initialize Passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Passport Google Strategy
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://ap-ecc-express.onrender.com/auth/google/callback'
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    const email = profile.emails[0].value;
-    let user = await User.findOne({ email });
-
-    if (!user) {
-      user = await User.create({
-        username: profile.displayName,
-        email: email,
-      });
-    }
-
-    return done(null, user);
-  } catch (error) {
-    return done(error, null);
-  }
-}));
-
-// Serialize and deserialize user
-passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
-
 mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
@@ -77,35 +23,6 @@ app.use('/', indexRoute);
 app.use('/test', testRoute);
 app.use('/messages', messagesRoute);
 app.use('/users', userRoute);
-
-// Google Authentication Routes
-app.get('/auth/google', (req, res, next) => {
-  const redirectUri = req.query.redirectUri;
-
-  const authOptions = {
-    scope: ['profile', 'email'],
-    state: JSON.stringify({ redirectUri }) // Encode redirectUri in state
-  };
-  
-  passport.authenticate('google', authOptions)(req, res, next);
-});
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
-  (req, res) => {
-    const user = req.user;
-    const { redirectUri } = JSON.parse(req.query.state); // Retrieve from state
-    const fallbackUri = 'exp://localhost:19000';
-
-    const userInfo = {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-    };
-
-    const redirectUrl = `${redirectUri || fallbackUri}?user=${encodeURIComponent(JSON.stringify(userInfo))}`;
-    res.redirect(redirectUrl);
-  }
-);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
